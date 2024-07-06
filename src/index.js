fc049b96749319c7554355ad1e2ff1f400a262d3
Asyl <duldievasylbek@gmail.com>
2024-07-06 23:41:00 +0500

    Random-character

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import MarvelService from './services/MarvelService';
-import './style/style.scss';
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
 
-const marvelService = new MarvelService();
+import "./style/style.scss";
 
-marvelService.getAllCharacters().then(res=> console.log(res))
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,18 +1,35 @@
-
-
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+
+    _akiKey = 'apikey=d2e5448ff4da4e09a732da20445bf084'
+
     getResource = async (url) => {
-        let res = await fetch(url);
+    let res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
+        return await res.json();
+    }   
+
     getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=d2e5448ff4da4e09a732da20445bf084');
+      return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._akiKey}`);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._akiKey}`);
+        return this._trarnsformCharacter(res)
+    }
+    _trarnsformCharacter = (res) => {
+        return {
+            name: res.data.results[0].name,
+            description: res.data.results[0].description,
+            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+            homepage: res.data.results[0].urls[0].url,
+            wiki: res.data.results[0].urls[1].url   
+        }
+
     }
 }
 
