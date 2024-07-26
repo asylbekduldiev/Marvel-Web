cb414aa42a2b0569799bb18b0b35a85ab595734c
Asyl <duldievasylbek@gmail.com>
2024-07-26 21:59:30 +0500

    info

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -28,9 +28,9 @@ class MarvelService {
             description: char.description,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url   
+            wiki: char.urls[1].url
         }
-
+        
     }
 }
 
