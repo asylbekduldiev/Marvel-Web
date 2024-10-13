f1da4e39975f3bbff72147faf3dd660939863de0
Asyl <duldievasylbek@gmail.com>
2024-10-13 22:04:19 +0500

    Empty

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,7 +9,7 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
 
     const [char, setChar] = useState(null);
-    const {loading, error, getCharacter, clearError} = useMarvelService();
+    const {loading, error, getCharacter} = useMarvelService();
 
     useEffect(() => {
         updateChar();
@@ -25,7 +25,6 @@ const RandomChar = () => {
     }
 
     const updateChar = () => {
-        clearError();
         const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
         getCharacter(id)
             .then(onCharLoaded);
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
-    const {loading, request, error, clearError} = useHttp()
+    const {loading, request, error} = useHttp()
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
 
@@ -40,6 +40,7 @@ const useMarvelService = () => {
         }
 
     }
+    return {loading, error, getAllCharacters, getCharacter}
 }
 
-export default MarvelService;
+export default useMarvelService;
